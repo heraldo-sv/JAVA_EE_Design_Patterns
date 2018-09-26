9bb225d7e0712861d0a1d6d624f4f64274b0c25f
Heraldo <heraldo.delvalle@gmail.com>
2018-09-26 11:19:20 -0600

    Facade
    Facade

---
+++ Facade/src/com/heraldo/App.java
@@ -0,0 +1,13 @@
+package com.heraldo;
+
+import com.heraldo.facade.CheckFacade;
+
+public class App {
+	public static void main(String[] args) {
+		CheckFacade cliente1 = new CheckFacade();
+		cliente1.buscar("02/07/2018", "08/07/2018", "Lima", "Cancún");
+		
+		CheckFacade cliente2 = new CheckFacade();
+		cliente2.buscar("02/07/2018", "08/07/2018", "Lima", "Quito");
+	}
+}
---
+++ Facade/src/com/heraldo/facade/CheckFacade.java
@@ -0,0 +1,21 @@
+package com.heraldo.facade;
+
+import com.heraldo.api.AvionAPI;
+import com.heraldo.api.HotelAPI;
+
+public class CheckFacade {
+
+	private AvionAPI avionAPI;
+	private HotelAPI hotelAPI;	
+	
+	public CheckFacade() {
+		avionAPI = new AvionAPI();
+		hotelAPI = new HotelAPI();
+	}
+	
+	public void buscar(String fechaIda, String fechaVuelta, String origen, String destino) {
+		avionAPI.buscarVuelos(fechaIda, fechaVuelta, origen, destino);
+		hotelAPI.buscarHoteles(fechaIda, fechaVuelta, origen, destino);
+	}
+
+}
